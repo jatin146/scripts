0f6b2f90c99f5b61976dc3101db9656ff3145dd2
Jatin Shah <jatins@netflix.com>
2018-12-18 16:27:41 -0800

    Print out the version of the Java installed after install is complete

--- shell/install-java.sh
+++ shell/install-java.sh
@@ -2,3 +2,13 @@
 
 sudo apt install openjdk-11-jre-headless
 sudo apt install openjdk-11-jdk-headless
+
+
+# Also print the versions of the Java and Java compiler installed
+echo "JAVA VERSION"
+echo "============"
+java -version
+
+echo "JAVAC VERSION"
+echo "============="
+javac -version
