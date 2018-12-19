c7dfbe8c078e96f1ec0f787fff565bbc136fd399
Jatin Shah <jatins@netflix.com>
2018-12-18 16:36:10 -0800

    Sourse the sdkman init script

--- shell/install-sdkman.sh
+++ shell/install-sdkman.sh
@@ -5,5 +5,8 @@ sudo apt-get install zip
 #!/bin/bash
 curl -s "https://get.sdkman.io" | bash
 
+# Source the sdkman init
+source ~/.sdkman/bin/sdkman-init.sh
+
 # Print out the installed version of sdkman
 sdk version
