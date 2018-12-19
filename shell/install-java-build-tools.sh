663ad9d9a21ddcd5509e7e9537559805b9e3f793
Jatin Shah <jatins@netflix.com>
2018-12-18 16:51:18 -0800

    Install Java Build Tools

---
+++ shell/install-java-build-tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if sdkman exists
+if [ ! -d "$HOME/.sdkman" ]; then
+  echo "SDKMAN does not exist"
+  echo "Please run install-sdkman.sh before continuing further"
+  exit 1
+fi
+
+# Install gradle, maven and ant
+echo "Installing ANT"
+#sdk install ant
+
+echo "Installing Maven"
+#sdk install maven
+
+echo "Installing Gradle"
+#sdk install gradle
+
