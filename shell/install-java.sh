254d00d3190cc1206386e0eae026f60e069ecc01
Jatin Shah <jatins@netflix.com>
2020-11-24 21:03:20 -0800

    Update install-java.sh

--- shell/install-java.sh
+++ shell/install-java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt install openjdk-11-jre-headless
-sudo apt install openjdk-11-jdk-headless
+sudo apt install openjdk-11-jre-headless -y
+sudo apt install openjdk-11-jdk-headless -y
 
 
 # Also print the versions of the Java and Java compiler installed
